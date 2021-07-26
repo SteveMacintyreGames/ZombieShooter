0baea676fe9677e800d00bb852e5864da2e6a5ac
Esteban Ibarra <SteveMacintyre868@gmail.com>
2021-07-25 18:01:12 -0700

    Players hands now stick to the gun, hid the head for better first person view.

---
+++ Assets/Scripts/SmoothDamp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothDamp : MonoBehaviour
+{
+   public Transform target;
+   public float speed = 10f;
+
+   void LateUpdate()
+   {
+       Vector3 handPos = target.transform.position;
+       transform.position = Vector3.Lerp(transform.position, handPos, Time.deltaTime * speed);
+       transform.rotation = Quaternion.Euler(target.transform.rotation.eulerAngles);
+   }
+}
